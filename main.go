370a7d0e96ac535db4f6cb2ecfc79aebd5d0a986
SSSo11o <ibronovaziz5@gmail.com>
2024-12-24 18:13:54 +0500

    Удаление все коды

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"servers/logger_1"
-)
-
 func main() {
-	var logger = logger_1.InitLogger()
 
-	router := gin.Default()
-	router.GET("/Hi", func(c *gin.Context) {
-		logger.Info("Получен запрос на /")
-		c.JSON(http.StatusOK, gin.H{"message": "Hello, Logger!"})
-	})
-
-	if err := router.Run(":8080"); err != nil {
-		logger.Error("Ошибка при запуске сервера:", err)
-	}
 }
