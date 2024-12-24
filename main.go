087de2545cb657dce4aed24c06024437b9b8ad0d
SSSo11o <ibronovaziz5@gmail.com>
2024-12-24 18:19:26 +0500

    Изминение в main

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"servers/logger"
-)
-
 func main() {
-	var logger = logger.InitLumberjackLogger()
-	router := gin.Default()
-	router.GET("/Hi", func(c *gin.Context) {
-		logger.Info("Получен запрос на /")
-		c.JSON(http.StatusOK, gin.H{"message": "Hello, logger!"})
-	})
-	if err := router.Run(":8080"); err != nil {
-		logger.Error("Ошибка при запуске сервера:", err)
-	}
+
 }
