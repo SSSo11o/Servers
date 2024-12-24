7aaff5ee6a73cad4f9fd4df6b20406bc721dc6a9
SSSo11o <ibronovaziz5@gmail.com>
2024-12-24 13:29:47 +0500

    Использование Logrus для логирования

---
+++ logger/lumberjack-logger.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+	"github.com/natefinch/lumberjack"
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func InitLumberjackLogger() *logrus.Logger {
+	log = logrus.New()
+	log.SetFormatter(&logrus.JSONFormatter{})
+	log.SetLevel(logrus.InfoLevel)
+
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   "./logs/app.log",
+		MaxSize:    10,
+		MaxBackups: 3,
+		MaxAge:     28,
+	})
+	return log
+}
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"servers/logger"
+)
 
+func main() {
+	var logger = logger.InitLogger()
+
+	router := gin.Default()
+	router.GET("/Hi", func(c *gin.Context) {
+		logger.Info("Получен запрос на /")
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, Logger!"})
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		logger.Error("Ошибка при запуске сервера:", err)
+	}
 }
